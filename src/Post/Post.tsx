060124e8b7e8988bfba472b44f2848ab3ff584dd
NikolosD <alcari2002@gmail.com>
2023-07-18 14:41:39 +0400

    Comit 43

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Post = () =>{
     return(
         <div>
-            <h2>user233</h2>
+            <h2>user2</h2>
         </div>
     )
 }
--- src/User/User.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const User = () =>{
-    return(
-        <div>
-            <h2>user233</h2>
-        </div>
-    )
-}
